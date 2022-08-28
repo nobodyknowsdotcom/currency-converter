c3f76d1ce57e2763e65b1907165244f7a01bfa11
Dmitry <myclosestisa@gmail.com>
2022-08-29 00:08:31 +0500

    Initial commit

---
+++ src/main/java/nobodyknowsdotcom/currencyconverterfeign/CurrencyConversionBean.java
@@ -0,0 +1,20 @@
+package nobodyknowsdotcom.currencyconverterfeign;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CurrencyConversionBean {
+    private int id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private int port;
+}
---
+++ src/main/java/nobodyknowsdotcom/currencyconverterfeign/CurrencyConverterFeignApplication.java
@@ -0,0 +1,13 @@
+package nobodyknowsdotcom.currencyconverterfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterFeignApplication.class, args);
+    }
+
+}
