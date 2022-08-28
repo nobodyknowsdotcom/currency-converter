f6b834652ec312135a33c99cdc395a3bfbac0e9e
Dmitry <myclosestisa@gmail.com>
2022-08-29 01:38:24 +0500

    Add Feign proxy

--- src/main/java/nobodyknowsdotcom/currencyconverterfeign/CurrencyConverterFeignApplication.java
+++ src/main/java/nobodyknowsdotcom/currencyconverterfeign/CurrencyConverterFeignApplication.java
@@ -2,8 +2,12 @@ package nobodyknowsdotcom.currencyconverterfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("nobodyknowsdotcom.currencyconverterfeign")
+@EnableDiscoveryClient
 public class CurrencyConverterFeignApplication {
 
     public static void main(String[] args) {
